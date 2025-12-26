fed0b7ffec649354f4ed7a13c9d3ac320e72e374
Nevroz Arslan <newrozarslan@gmail.com>
2025-12-26 16:39:56 +0300

    refact: Default template includes more lines

--- lib/blendend_playground_phx_web/live/playground_live.ex
+++ lib/blendend_playground_phx_web/live/playground_live.ex
@@ -214,7 +214,9 @@ defmodule BlendendPlaygroundPhxWeb.PlaygroundLive do
 
   defp default_code do
     """
-    draw 420, 240 do
+    width = 420
+    height = 420
+    draw width, height do
       clear(fill: rgb(242, 230, 216))
       rect(40, 40, 160, 80, fill: rgb(58, 102, 152))
       font = load_font("priv/fonts/AlegreyaSans-Regular.otf", 18)
